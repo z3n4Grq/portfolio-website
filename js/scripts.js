031ad5a6a5c130caa3d701ece1cb72f8c16ae67d
z3n4Grq <80496641+z3n4Grq@users.noreply.github.com>
2021-04-21 13:18:34 +1000

    Update scripts.js
    Added menu toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there, welcome to my site")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
