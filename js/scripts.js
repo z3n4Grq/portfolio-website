2650bc006f775a68132f47adfb0283837703869d
z3n4Grq <80496641+z3n4Grq@users.noreply.github.com>
2021-05-10 21:55:46 +1000

    UPdated

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hi there, welcome to my site")
+console.log('Hi there, welcome to my site')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
